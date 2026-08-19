51cf916dc25a69d3c2f28be9d389db4ac3c6f179
mesachinnagane <sachinnagane01@gmail.com>
2026-08-19 14:22:51 +0530

    collection files add by kuldip

---
+++ NestedList.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class NestedList
+{
+   public static void main(String[] args)
+   {
+       List<String> Mah = new ArrayList<String>();
+       Mah.add("Pune");
+       Mah.add("Mumbai");
+       Mah.add("Nagpur");
+       Mah.add("Jalgaon");
+       Mah.add("Nashik");
+
+     List<String> Guj = new ArrayList<>();
+        Guj.add("Surat");
+        Guj.add("Ahemdabad");
+        Guj.add("Rajkot");
+        Guj.add("Vadodara");
+
+    List<List<String>>  State = new ArrayList<>();
+      State.add(Mah);
+      State.add(Guj);
+
+        List<List<List<String>>> India = new ArrayList<>();
+         India.add(State);
+
+          System.out.println(India);
+
+    for( List<List<String>> s1 : India)
+    {
+         for(List<String> s2 : s1)
+          { 
+               for(String s3 : s2)
+
+                {                       
+                System.out.println(s3+" ");
+                }
+          }
+    }
+ 
+   
+
+  }
+
+}
